d90482cbc9f2422d2eae5e374b5bef6dc8c3084f
chsi-odoo <chsi@odoo.com>
2025-03-21 15:23:21 +0800

    feat: add unique constraints for sale branch

--- sale_branch/models/sale_branch.py
+++ sale_branch/models/sale_branch.py
@@ -5,8 +5,13 @@ class SaleBranch(models.Model):
     _name = 'sale.branch'
     _description = "Branch for sales"
 
+    _sql_constraints = [
+        ('name_unique', 'UNIQUE(name)', "Name must be unique"),
+        ('code_unique', 'UNIQUE(code)', "Code must be unique"),
+    ]
+
     name = fields.Char(required=True)
-    sequence_id  = fields.Many2one("ir.sequence")
+    sequence_id = fields.Many2one("ir.sequence")
     code = fields.Char(required=True)
 
     @api.model
