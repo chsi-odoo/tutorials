3aa8d8835ac1e3743a9b4728bfc3fd208462a8ad
chsi-odoo <chsi@odoo.com>
2025-03-25 18:10:00 +0800

    feat: add solutions for report case study (TODO: load css)

---
+++ better_report/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Better Reports',
+    'version': '0.0.1',
+    'depends': ['portal', 'account'],
+    'data': [
+        'report/portal_reports.xml',
+        'report/portal_templates.xml',
+        'report/invoice_templates.xml',
+        'views/res_partner_views.xml',
+    ],
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'static/src/css/better_report_styles.css',
+    #         'better_report/static/src/css/better_report_styles.css',
+    #     ],
+    #     'web.assets_common': [
+    #         'static/src/css/better_report_styles.css',
+    #         'better_report/static/src/css/better_report_styles.css',
+    #     ]
+    # }
+}
