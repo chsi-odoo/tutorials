83effeb18f4240408b078fd3888f8b62dc4ea838
chsi-odoo <chsi@odoo.com>
2025-03-28 10:46:35 +0800

    temp commit

---
+++ better_pos/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Better POS',
+    'version': '0.0.1',
+    'depends': ['point_of_sale'],
+    'assets': {
+        # 'better_pos.assets_common': [
+        #     'better_pos/static/src/*',
+        #     'better_pos/static/src/**/*',
+        # ],
+        'point_of_sale._assets_pos': [
+            'better_pos/static/src/*',
+            'better_pos/static/src/**/*',
+        ]
+    }
+}
