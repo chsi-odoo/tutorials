93fe28e28fe23d19728005b4eb5ac2b9ab216e94
chsi-odoo <chsi@odoo.com>
2025-03-31 16:35:08 +0800

    fix: change sales permission to dropdown

---
+++ estate/tests/test_estate.py
@@ -0,0 +1,15 @@
+from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
+from odoo.exceptions import UserError
+
+# @tagged('post_install', '-at_install')
+# class EstateTestCase(TransactionCase):
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         super().setUpClass()
+#
+#     def test_creation_area(self):
+#
+#
+#     def test_action_sell(self):
