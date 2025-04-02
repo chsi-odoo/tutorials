6914224e5201accf38390da9177603052159ab6e
chsi-odoo <chsi@odoo.com>
2025-04-02 16:01:50 +0800

    fix: fix import order, add missing accountant dependency, add default groups for estate_group_manager to prevent lock out

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Estate",
     'version': '0.0.1',
     'category': 'Real Estate/Brokerage',
-    'depends': ['base'],
+    'depends': ['base','accountant'],
     'author': "Me",
     'application': True,
     'description': """
@@ -10,20 +10,21 @@
     """,
     # data files always loaded at installation
     'data': [
-        'security/ir.model.access.csv',
         'report/estate_property_templates.xml',
         'report/estate_property_reports.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/estate_menus.xml',
         "views/res_user_views.xml",
         'data/estate.property.type.csv',
         'data/estate.property.csv',
         'data/estate_property_offer.xml',
         'data/estate_property.xml',
-        'security/security.xml',
+
     ],
     # # data files containing optionally loaded demonstration data
     'demo': [
